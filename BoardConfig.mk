d081af81d7e10fff91a96d10016ba5f8164f8815
Alex Cruz <mazdarider23@gmail.com>
2014-08-09 23:24:44 -0500

    Move apq8064-common into device folder
    Change-Id: I63c7e8e9674a38afc5e18d3a082d1bd90ed8e478

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,8 +14,14 @@
 # limitations under the License.
 #
 
-# inherit from common apq8064
--include device/oppo/apq8064-common/BoardConfigCommon.mk
+# inherit from Oppo common
+-include device/oppo/common/BoardConfigCommon.mk
+
+# inherit from the proprietary version
+-include vendor/oppo/apq8064-common/BoardConfigVendor.mk
+
+# inherit from the proprietary version
+-include vendor/oppo/n1/BoardConfigVendor.mk
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80200000
@@ -43,9 +49,6 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
 BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 1073741824
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 3221209088  # 3221209088 - 16384 for crypto footer
 
-# inherit from the proprietary version
--include vendor/oppo/n1/BoardConfigVendor.mk
-
 # TWRP specific build flags
 DEVICE_RESOLUTION := 1080x1920
 #RECOVERY_SDCARD_ON_DATA := true
@@ -56,3 +59,129 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 #TW_MAX_BRIGHTNESS := 255
 #TW_BOARD_CUSTOM_GRAPHICS := ../../../device/oppo/n1/recovery/graphics.c
 TW_NO_SCREEN_TIMEOUT := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8960
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8960
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := krait
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+
+# Flags
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP -DNO_SECURE_DISCARD
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_USES_QCOM_BSP := true
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+TARGET_QCOM_AUDIO_VARIANT := caf
+TARGET_QCOM_DISPLAY_VARIANT := caf
+TARGET_QCOM_MEDIA_VARIANT := caf
+BOARD_USES_QC_TIME_SERVICES := true
+TARGET_NO_RPC := true
+
+# Audio
+BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
+BOARD_HAVE_CSD_FAST_CALL_SWITCH := true
+BOARD_USES_ALSA_AUDIO := true
+BOARD_USES_LEGACY_ALSA_AUDIO := true
+BOARD_USES_FLUENCE_FOR_VOIP := true
+BOARD_USES_FLUENCE_INCALL := true
+BOARD_USES_SEPERATED_AUDIO_INPUT := true
+BOARD_USES_SEPERATED_HEADSET_MIC := true
+BOARD_USES_SEPERATED_VOICE_SPEAKER := true
+TARGET_USES_QCOM_COMPRESSED_AUDIO := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+
+# Camera
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# Graphics
+BOARD_EGL_CFG := device/oppo/n1/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+TARGET_DISPLAY_USE_RETIRE_FENCE := true
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_ION := true
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+HAVE_ADRENO_SOURCE:= false
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Radio
+BOARD_PROVIDES_LIBRIL := true
+TARGET_ADDITIONAL_BOOTCLASSPATH := qcnvitems:qcrilhook
+
+# Webkit
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
+
+# Wifi
+BOARD_HAS_QCOM_WLAN              := true
+BOARD_WLAN_DEVICE                := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_qcwcn
+TARGET_USES_WCNSS_CTRL           := true
+TARGET_USES_QCOM_WCNSS_QMI       := true
+TARGET_WCNSS_MAC_PREFIX          := e8bba8
+WIFI_DRIVER_FW_PATH_STA          := "sta"
+WIFI_DRIVER_FW_PATH_AP           := "ap"
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+
+# File system
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_VOLD_MAX_PARTITIONS := 30
+
+# Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_RECOVERY_SWIPE := true
+ifeq ($(WITH_SIMPLE_RECOVERY),true)
+    PRODUCT_EXTRA_RECOVERY_KEYS += vendor/extra/recovery_keys/OPPO_N1
+    TARGET_RECOVERY_FSTAB := device/oppo/n1/recovery.fstab
+else
+    TARGET_RECOVERY_FSTAB := device/oppo/n1/rootdir/etc/fstab.qcom
+endif
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/oppo/n1/sepolicy
+BOARD_SEPOLICY_UNION += \
+       app.te \
+       bluetooth.te \
+       device.te \
+       domain.te \
+       drmserver.te \
+       file.te \
+       file_contexts \
+       hci_init.te \
+       init_shell.te \
+       keystore.te \
+       mediaserver.te \
+       kickstart.te \
+       nfc.te \
+       rild.te \
+       surfaceflinger.te \
+       system.te \
+       ueventd.te \
+       wpa.te
--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+VENDOR=oppo
+DEVICE=n1
 
-export DEVICE=n1
-export VENDOR=oppo
-./../apq8064-common/extract-files.sh $@
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $BASE/*
+
+for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
+    OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+    FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+    DEST=${PARSING_ARRAY[1]}
+    if [ -z $DEST ]
+    then
+        DEST=$FILE
+    fi
+    DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+    fi
+    # Try CM target first
+    adb pull /system/$DEST $BASE/$DEST
+    # if file does not exist try OEM target
+    if [ "$?" != "0" ]
+    then
+        adb pull /system/$FILE $BASE/$DEST
+    fi
+done
